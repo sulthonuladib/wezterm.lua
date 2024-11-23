45c7ed7eac082a3672a1617a47bd44a33a32b6cb
sulthonuladib <adibcyber4rt@gmail.com>
2024-11-23 19:48:33 +0700

    superduperman

--- wezterm.lua
+++ wezterm.lua
@@ -1,33 +1,52 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local action = wezterm.action
 
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
+
+config.font = wezterm.font("JetBrains Mono")
+-- config.font = wezterm.font("Fira Code")
 
 config.enable_scroll_bar = false
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
--- config.tab_bar_at_bottom = true
-config.enable_tab_bar = false
-config.window_decorations = "NONE"
-config.color_scheme = 'tokyonight-storm'
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 64
+-- config.enable_tab_bar = false
+-- config.window_decorations = "NONE"
+
+-- config.color_scheme = "Tokyo Night"
+config.color_scheme = "Tomorrow Night Eighties"
+
+config.warn_about_missing_glyphs = false
 
 config.keys = {}
 
 for i = 1, 8 do
-  -- CTRL+ALT + number to move to that position
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = wezterm.action.MoveTab(i - 1),
-  })
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = action.ActivateTab(i - 1),
+	})
 end
 
 return config
